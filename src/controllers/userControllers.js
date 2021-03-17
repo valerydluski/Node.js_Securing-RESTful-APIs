9455ec2731c8af6c74dd8e493a76caee13974f3d
Valery Dluski <valerydluski@gmail.com>
2021-03-17 20:45:02 +0300

    feat: Add the handlers for login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -25,3 +25,28 @@ export const register = (req, res) => {
     }
   });
 };
+
+export const login = (req, res) => {
+  User.findOne(
+    {
+      email: req.body.email,
+    },
+    (err, user) => {
+      if (err) throw err;
+      if (!user) {
+        res.status(401).json({ message: 'Authentication failed. No user found' });
+      } else if (user) {
+        if (!user.comparePassword(req.body.password, user.hashPassword)) {
+          res.status(401).json({ message: 'Authentication failed. Wrong password' });
+        } else {
+          return res.json({
+            token: jwt.sign(
+              { email: user.email, username: user.username, _id: user.id },
+              'RESTFULAPIs'
+            ),
+          });
+        }
+      }
+    }
+  );
+};
