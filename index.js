9af42ecd7d6ce7bc3d0b74d31392c7f4c93d16a7
Valery Dluski <valerydluski@gmail.com>
2021-03-18 09:23:00 +0300

    fix: fix bug

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ app.use(bodyParser.json());
 
 //JWT setup
 app.use((req, res, next) => {
-  if (req.headers?.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(' ')[0] === 'JWT'
+  ) {
     jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', (err, decode) => {
       if (err) req.user = undefined;
       req.user = decode;
